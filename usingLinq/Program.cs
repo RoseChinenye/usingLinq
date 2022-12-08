ff0e8bc47ff10822eb3f8836df6fc8b88878298f
Chinenye <c.okeke@genesystechhub.com>
2022-12-08 16:33:50 +0100

    Add project files.

---
+++ usingLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace usingLinq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*practice p = new practice();
+        p.getPractice();*/
+        LambdaExpressionSyntax();
+        
+    }
+   public static void LambdaExpressionSyntax()
+    {
+        // Make a list of integers.
+        List<int> list = new List<int>();
+        list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+       
+
+        var ListOfEvenNumbers = list.FindAll(x => (x % 2) == 0);
+        foreach(var s in ListOfEvenNumbers)
+        {
+            Console.WriteLine(s);
+        }
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
